c93d843ac6fa226b82bb0086c59bbe7d7ea967fd
amquach00 <76972833+amquach00@users.noreply.github.com>
2024-02-16 12:41:28 -0800

    #25 add settings page

--- app/src/main/java/com/example/activeamigo/ui/preferences/PreferencesViewModel.java
+++ app/src/main/java/com/example/activeamigo/ui/preferences/PreferencesViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class PreferencesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public PreferencesViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is preferences fragment");
     }
