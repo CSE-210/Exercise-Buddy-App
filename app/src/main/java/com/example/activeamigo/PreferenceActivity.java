8d71878fa32295d254fca80eca25dcbadf9503f6
Calvin <cknguyen@ucsd.edu>
2024-02-18 11:34:26 -0800

    #27 - gettings and setting data from/to firebase Made other UI fixes and loading from database

--- app/src/main/java/com/example/activeamigo/PreferenceActivity.java
+++ app/src/main/java/com/example/activeamigo/PreferenceActivity.java
@@ -1,31 +1,75 @@
 package com.example.activeamigo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class PreferenceActivity extends AppCompatActivity {
 
     private EditText dobEditText;
+    private Button edit_save;
+    private Spinner exercise_choice;
+    private Spinner location_choice;
+    private RadioGroup genderGroup;
+    private String gender;
+    private EditText bio;
+
+
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_preferences);
-        Log.d("fff","Yes Click");
+        edit_save = findViewById(R.id.save);
+        edit_save.setText("Save");
+
+        //View variables
+        exercise_choice = findViewById(R.id.exercise);
+        location_choice = findViewById(R.id.location);
+        genderGroup = findViewById(R.id.gender_radio_group);
+        bio = findViewById(R.id.bio);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true); // showing the back button in action bar
 
         dobEditText = findViewById(R.id.dob_edit_text);
-
         dobEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +77,8 @@ public class PreferenceActivity extends AppCompatActivity {
             }
         });
 
+
+        /** Modifying Drop Down Arrays **/
         //Go to strings.xml under activity_array to change/add drop down options
         Spinner exerciseSpinner = findViewById(R.id.exercise);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -49,10 +95,68 @@ public class PreferenceActivity extends AppCompatActivity {
         locationSpinner.setAdapter(adapter2);
         locationSpinner.setSelection(adapter2.getPosition("Select Location"));
 
+        // Gender Selection
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                // on below line we are getting radio button from our group.
+                RadioButton radioButton = findViewById(checkedId);
+
+                gender = radioButton.getText().toString();
+
+            }
+        });
+
+        /** Save Button Action **/
+        // TODO: Save -> put into firebase table
+        // TODO: Modify collections path based off user here*
+        edit_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO:1. Check if all info is filled, then if conditional to the rest
+
+                // 2. Save info to database
+                DocumentReference docRef = db.collection("Emails").document("Preference");
+
+                // Create a HashMap to store the data
+                Map<String, Object> data = new HashMap<>();
+                data.put("exercise",exercise_choice.getSelectedItem().toString());
+                data.put("location", location_choice.getSelectedItem().toString());
+                data.put("gender", gender);
+                data.put("dob",dobEditText.getText().toString());
+                data.put("bio",bio.getText().toString());
+
+                // Set the data to the document with document ID "LA"
+                docRef.set(data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(PreferenceActivity.this, "Preferences Saved!", Toast.LENGTH_LONG).show();
+
+                                Log.d("API", "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(PreferenceActivity.this, "Preferences Failed!", Toast.LENGTH_LONG).show();
+
+                                Log.w("API", "Error writing document", e);
+                            }
+                        });
+
+                //Exit
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
+
+
 
     }
 
-
+    /** Calender pop up to select DOB **/
     private void showDatePickerDialog() {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
@@ -77,7 +181,32 @@ public class PreferenceActivity extends AppCompatActivity {
 
         // Set the maximum date allowed to the current date
         datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-
         datePickerDialog.show();
+    }
+
+    /** Back button: function is not to save edited data to firebase **/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Enable XML Views**/
+    public void enableAllViews(ViewGroup viewGroup) {
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            View child = viewGroup.getChildAt(i);
+            // Enable clickable attribute
+            child.setClickable(true);
+            child.setFocusable(true);
+
+            if (child instanceof ViewGroup) {
+                enableAllViews((ViewGroup) child);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/activeamigo/ui/preferences/PreferencesViewModel.java
+++ app/src/main/java/com/example/activeamigo/ui/preferences/PreferencesViewModel.java
@@ -1,29 +1,111 @@
 package com.example.activeamigo.ui.preferences;
 
+import android.util.Log;
+import android.widget.RadioButton;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.activeamigo.R;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class PreferencesViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-    private MutableLiveData<String> dateOfBirth = new MutableLiveData<>();
+    private MutableLiveData<String> dateOfBirth;
+    private MutableLiveData<String> bio;
+    private MutableLiveData<String> gender = new MutableLiveData<>();
+    private MutableLiveData<String> exercise = new MutableLiveData<>();
+    private MutableLiveData<String> location = new MutableLiveData<>();
+
+    private MutableLiveData<String> firebaseData = new MutableLiveData<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public PreferencesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("My Account");
+        dateOfBirth = new MutableLiveData<>();
+        bio = new MutableLiveData<>();
 
-    }
-
-    public LiveData<String> getText() {
-        return mText;
+        fetchDataFromFirebase();
     }
 
     public LiveData<String> getDateOfBirth() {
-        return dateOfBirth;
 
+        return dateOfBirth;
     }
+    public LiveData<String> getBio() {
+
+        return bio;
+    }
+    public LiveData<String> getGender() {
+
+        return gender;
+    }
+    public LiveData<String> getExercise() {
+        return exercise;
+    }
+    public LiveData<String> getLocation() {
+        return location;
+    }
+
+    public LiveData<String> getFirebaseData() {
+        return firebaseData;
+    }
+
     public void setDateOfBirth(String dob) {
+
         dateOfBirth.setValue(dob);
     }
+    public void setBio(String bio_text) {
+
+        bio.setValue(bio_text);
+    }
+    public void setGender(String g) {
+
+        gender.setValue(g);
+    }
+    public void setExercise(String ex) {
+        exercise.setValue(ex);
+    }
+    public void setLocation(String loc) {
+        location.setValue(loc);
+    }
+
+    /**TODO: Modify collections path based off user here**/
+    public void fetchDataFromFirebase() {
+        // Fetch data from Firebase
+        DocumentReference docRef = db.collection("Emails").document("Preference");
+        docRef.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                // If document exists, fetch data
+                String exerciseText = documentSnapshot.getString("exercise");
+                String locationText = documentSnapshot.getString("location");
+                String genderText = documentSnapshot.getString("gender");
+                String dobText = documentSnapshot.getString("dob");
+                String bioText = documentSnapshot.getString("bio");
+
+                // Concatenate fetched data into a single string
+                String firebaseDataString = "Exercise: " + exerciseText + "\n" +
+                        "Location: " + locationText + "\n" +
+                        "Gender: " + genderText + "\n" +
+                        "DOB: " + dobText + "\n" +
+                        "Bio: " + bioText;
+                Log.d("API", "ViewModel" + bioText+ " " + dobText + " " + genderText + " " + locationText + " " + exerciseText);
+                // Update LiveData with fetched data
+//                firebaseData.setValue(firebaseDataString);
+
+                setDateOfBirth(dobText);
+                setBio(bioText);
+                setGender(genderText);
+                setExercise(exerciseText);
+                setLocation(locationText);
+
+
+            }
+        }).addOnFailureListener(e -> {
+            // Handle failure
+        });
+    }
+
+
 }
