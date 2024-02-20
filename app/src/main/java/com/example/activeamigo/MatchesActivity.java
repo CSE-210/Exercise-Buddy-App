b62e5b9135a324ff95c46df2925041c34489e627
LoukikNaik <loukiknaik@gmail.com>
2024-02-19 17:09:02 -0800

    Matches UI and dynamically adding matches to the UI. No Db connection yet

---
+++ app/src/main/java/com/example/activeamigo/MatchesActivity.java
@@ -0,0 +1,19 @@
+package com.example.activeamigo;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.checkerframework.checker.units.qual.A;
+import com.example.activeamigo.ui.matches.MatchesViewModel;
+import java.util.ArrayList;
+//import matchesViewModelupdateList
+public class MatchesActivity extends AppCompatActivity {
+//    private ListView matchesList;
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_matches);
+    }
+}
--- app/src/main/java/com/example/activeamigo/ui/matches/MatchesPage.java
+++ app/src/main/java/com/example/activeamigo/ui/matches/MatchesPage.java
@@ -1,20 +1,42 @@
 package com.example.activeamigo.ui.matches;
+//package com.example.activeamigo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.activeamigo.Algorithm;
+import com.example.activeamigo.MatchesActivity;
+//import com.example.activeamigo.ui.matches.R;
+import com.example.activeamigo.R;
 import com.example.activeamigo.databinding.FragmentMatchesBinding;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.util.Log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MatchesPage extends Fragment {
 
     private FragmentMatchesBinding binding;
-
+    private ListView matchesList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MatchesViewModel matchesViewModel =
@@ -22,10 +44,111 @@ public class MatchesPage extends Fragment {
 
         binding = FragmentMatchesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+//
+//        final TextView textView = binding.textMatches;
+//        matchesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        matchesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        final TextView textView = binding.textMatches;
-        matchesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        matchesViewModel.getMyListLiveData().observe(getViewLifecycleOwner(), new Observer<ArrayList<String>>() {
+            @Override
+            public void onChanged(ArrayList<String> strings) {
+                matchesList = root.findViewById(R.id.matchesList);
+                ArrayAdapter<String> matchesAdaptor = new ArrayAdapter<>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        strings
+                );
+                matchesList.setAdapter(matchesAdaptor);
+            };
+//
+        });
+        //Need to get the current user i.e. user1 info from database
+        //Need to get all data map and users list from the database
+        String user1 = "wafybtvgupqefg";
+        File file = new File("/Users/loukiknaik/Downloads/sample1_1.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+//        Map<String, List<Object>> data = objectMapper.readValue(file, Map.class);
+        Map<String, List<Object>> data = new HashMap<>();
+        try {
+            data = objectMapper.readValue(file, Map.class);
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception (e.g., log it, throw a runtime exception, etc.)
+        }
+        List<Object> objectList = new ArrayList<>(
+                Arrays.asList(1, Arrays.asList(5,8,9,11,12,15,16,17,21))
+        );
+        data.put("wafybtvgupqefg",objectList);
+        List<Object> objectList1 = new ArrayList<>(
+                Arrays.asList(6, Arrays.asList(2, 4, 10, 14, 20, 22))
+        );
+        data.put("keyxtaamnivy",objectList1);
+        List<Object> objectList2 = new ArrayList<>(
+                Arrays.asList(1, Arrays.asList(5,8,9,11,12,15,17,21))
+        );
+        data.put("lafybtvgupfg",objectList2);
+
+        List<String> users = new ArrayList<>(data.keySet());
+        ArrayList<String> matches = findMatches(user1,users,data);
+        matchesViewModel.setList(matches);
         return root;
+    }
+    public ArrayList<String> findMatches(String user1, List<String> users, Map<String, List<Object>> data){
+        ArrayList<String> rankedUsers = new ArrayList<String>();
+        Log.d("Start","Start userList creation");
+        List<List<Object>> userList = findSimilarSchedulesForUser(user1,users,data);
+        Log.d("Success","Passed userList creation");
+        int j=20;
+//        for(int i=0;i<100;i++){
+//            rankedUsers.add("Rahul");
+//            System.out.println("Rahul");
+//        }
+        for(List<Object> u: userList){
+            String s = u.get(1).toString();
+            System.out.println(s);
+            rankedUsers.add(s);
+            j-=1;
+            if(j==0){
+                break;
+            }
+        }
+        return rankedUsers;
+    }
+    public List<List<Object>> findSimilarSchedulesForUser(String user1, List<String> users, Map<String, List<Object>> data) {
+        Log.d("Start uswe List", "ejnf");
+        Log.d((data).toString(),"Data log");
+        List<Integer> user1Schedule = (List<Integer>) data.get(user1).get(1);
+        Log.d(user1Schedule.toString(),"user 1 schedule");
+        int user1Day = (int) data.get(user1).get(0);
+        Map<String, Integer> score = new HashMap<>();
+
+        for (String u : users) {
+            if (!data.containsKey(u)|| data.get(u) == null || data.get(u).size() < 2) continue;
+            int i = 0, j = 0;
+            if (user1Day != (int) data.get(u).get(0)) continue;
+
+            List<Integer> uSchedule = (List<Integer>) data.get(u).get(1);
+            int currScore = 0;
+            while (i < user1Schedule.size() && j < uSchedule.size()) {
+                if (user1Schedule.get(i).equals(uSchedule.get(j))) {
+                    currScore++;
+                    i++;
+                    j++;
+                } else if (user1Schedule.get(i) < uSchedule.get(j)) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+            score.put(u, currScore);
+        }
+
+        List<List<Object>> similarUserList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : score.entrySet()) {
+            similarUserList.add(Arrays.asList(entry.getValue(), entry.getKey()));
+        }
+        similarUserList.sort((a, b) -> (int) b.get(0) - (int) a.get(0));
+        return similarUserList;
     }
 
     @Override
