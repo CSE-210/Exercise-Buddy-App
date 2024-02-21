fb7c12984c698824082b393c1941db8a845959ae
amquach00 <76972833+amquach00@users.noreply.github.com>
2024-02-21 11:24:34 -0800

    add DAO class

---
+++ app/src/main/java/com/example/activeamigo/DAO.java
@@ -0,0 +1,14 @@
+package com.example.activeamigo;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.*;
+
+
+public class DAO {
+    // Access a Cloud Firestore instance from your Activity
+    static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void addAccount(){};
+
+    public void addCalendar(){};
+}
