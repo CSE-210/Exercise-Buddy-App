6f13b4918e65d4a8fd5290f972e3b7c3a286c17e
mjalbert99 <mjalbert@ucsd.edu>
2024-02-21 08:15:59 -0800

    Create login activity with button listeners

---
+++ app/src/main/java/com/example/activeamigo/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.activeamigo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class LoginActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Login Button Action
+        findViewById(R.id.buttonLogIn).setOnClickListener(view -> {
+
+
+        });
+
+        // Launch Register Page
+        findViewById(R.id.buttonRegister).setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+
+        });
+
+
+        // Launch forgot password
+        findViewById(R.id.forgotPasswordText).setOnClickListener(view -> {
+
+
+        });
+    }
+
+    // used to show dialogs
+    protected void showAlert(int messageId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setMessage(messageId)
+                .setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
