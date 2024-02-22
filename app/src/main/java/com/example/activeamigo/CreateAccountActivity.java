6e088105d042ac8beca2bf058b9bb5b2bd6f3d6b
mjalbert99 <mjalbert@ucsd.edu>
2024-02-22 12:02:39 -0800

    final data format

--- app/src/main/java/com/example/activeamigo/CreateAccountActivity.java
+++ app/src/main/java/com/example/activeamigo/CreateAccountActivity.java
@@ -1,26 +1,37 @@
 package com.example.activeamigo;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
-public class CreateAccountActivity extends AppCompatActivity {
-
+public class CreateAccountActivity extends AppCompatActivity implements Alertable {
+    private enum Day {Mon, Tue, Wed, Thu, Fri, Sat, Sun}
     protected FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         String fSName = getResources().getString(R.string.dbAccounts);
         db = FirebaseFirestore.getInstance();
+
+        // Setting up action bar
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         // When the button is clicked
         findViewById(R.id.buttonCreateAccount).setOnClickListener(view -> {
@@ -50,15 +61,15 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         // If a section is left empty
         if (name.isEmpty() || emailAddress.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()) {
-            showAlert(R.string.createAccountErrorFilledIn);
+            showAlert(this, R.string.createAccountErrorFilledIn);
         }
         // Bad email or non ucsd email
         else if (domainPos < 1 || !domain.equals(getString(R.string.ucsdDomain))) {
-            showAlert(R.string.createAccountErrorBadEmail);
+            showAlert(this, R.string.createAccountErrorBadEmail);
         }
         // If the passwords do not match
         else if (!password.equals(passwordConfirm)) {
-            showAlert(R.string.createAccountErrorMismatchPassword);
+            showAlert(this, R.string.createAccountErrorMismatchPassword);
             clearForm(true);
         }
         else return true;
@@ -66,59 +77,48 @@ public class CreateAccountActivity extends AppCompatActivity {
         return false;
     }
 
-    // Shows the dialog boxes based on the status's
-    protected void showAlert(int messageId) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(CreateAccountActivity.this);
-        builder.setMessage(messageId)
-                .setPositiveButton("OK", null);
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
     // Check if the email already exists in the database
     protected void checkEmail(String name, String emailAddress, String password, String dbName, FirebaseFirestore fs) {
         fs.collection(dbName)
-                .whereEqualTo("Email", emailAddress)
+                .whereEqualTo("email", emailAddress)
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         QuerySnapshot querySnapshot = task.getResult();
                         if (querySnapshot != null && !querySnapshot.isEmpty()) {
                             // Email already exists, show error message
-                            showAlert(R.string.accountCreationEmailExists);
+                            showAlert(this, R.string.accountCreationEmailExists);
                         } else {
                             // Email does not exist, proceed with adding the account
                             addAccount(name, emailAddress, password, dbName);
                         }
                     } else {
                         // Error occurred while checking for email existence
-                        showAlert(R.string.queryError);
+                        showAlert(this, R.string.queryError);
                     }
                 });
     }
 
     // Adds account to database
     protected void addAccount(String name, String emailAddress, String password, String dbName) {
-        HashMap<String, Object> accountData = new HashMap<>();
-        accountData.put("Name", name);
-        accountData.put("Email", emailAddress);
-        accountData.put("Password", password);
+        HashMap<String, Object> accountData = makeAccount(name, emailAddress, password);
+
 
         // Add the account data to db
         db.collection(dbName)
-                .document()
+                .document(emailAddress)
                 .set(accountData, SetOptions.merge())
                 // Success!
                 .addOnSuccessListener(aVoid -> {
-                    showAlert(R.string.accountCreationSuccess);
+                    showAlert(this, R.string.accountCreationSuccess);
                     clearForm(false);
                 })
                 // Failure :(
-                .addOnFailureListener(e -> showAlert(R.string.accountCreationFailed));
+                .addOnFailureListener(e -> showAlert(this, R.string.accountCreationFailed));
     }
 
-    // Used to clear the text inputs
-    // If true will only clear the passwords
+    /** Used to clear the text inputs
+     If true will only clear the passwords **/
     private void clearForm(boolean pass) {
         EditText editTextName = findViewById(R.id.editTextNameAC);
         EditText editTextEmailAddress = findViewById(R.id.editTextEmailAddressAC);
@@ -131,5 +131,39 @@ public class CreateAccountActivity extends AppCompatActivity {
         }
         editTextPassword.setText("");
         editTextPasswordConfirm.setText("");
+    }
+
+    /** Back button: function is not to save edited data to firebase **/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private HashMap<String, Object> makeAccount(String name, String emailAddress, String password){
+        HashMap<String, Object> res = new HashMap<>();
+        HashMap<String, Object> calendar = new HashMap<>();
+
+        res.put("name", name);
+        res.put("email", emailAddress);
+        res.put("password", password);
+        res.put("bio", "");
+        res.put("dob", "");
+        res.put("exercise", "");
+        res.put("gender", "");
+        res.put("location", "");
+        Day[] days = Day.values();
+        int numOfDays = 7;
+        for (int i = 0; i < numOfDays && i < days.length; i++) {
+            int numOfHours = 24;
+            calendar.put(days[i].toString(), new ArrayList<>(Collections.nCopies(numOfHours, 0)));
+        }
+        res.put("calendar", calendar);
+
+        return res;
     }
 }
