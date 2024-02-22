87c4974fa1c8011675e82c0102fb4054fa029a79
mjalbert99 <mjalbert@ucsd.edu>
2024-02-22 13:56:17 -0800

    makes call to grab account with email and check password

--- app/src/main/java/com/example/activeamigo/LoginActivity.java
+++ app/src/main/java/com/example/activeamigo/LoginActivity.java
@@ -3,40 +3,108 @@ package com.example.activeamigo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
-import androidx.appcompat.app.AlertDialog;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class LoginActivity extends Activity {
-
+public class LoginActivity extends Activity implements Alertable{
+    private FirebaseFirestore db = null;
+    private EditText emailView = null;
+    private EditText passwordView = null;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        db = FirebaseFirestore.getInstance();
+        String fsName = "Accounts";
 
-        // Login Button Action
+        emailView = findViewById(R.id.editTextEmailLG);
+        passwordView = findViewById(R.id.editTextpasswordLG);
+
         findViewById(R.id.buttonLogIn).setOnClickListener(view -> {
-            showAlert(R.string.press_test);
+            String email = emailView.getText().toString();
+            String password = passwordView.getText().toString();
 
+            // Checks that inputs are valid
+            if( !checkInputs(email, password)){
+                showAlert(this, R.string.createAccountErrorFilledIn);
+            }
+
+            // Log in attempt
+            else{
+                // Grabs account from database
+                Task<DocumentSnapshot> res = checkAccount(email, fsName, this.db);
+
+                res.addOnCompleteListener(task -> {
+                    // If it grabs an account successfully
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot documentSnapshot = task.getResult();
+                        // If the account exists
+                        if (documentSnapshot != null) {
+                            // Grab the password
+                            String passwordDB = documentSnapshot.getString("password");
+                            // ADD decryption
+
+                            // Check the password
+                            if (checkPassword(password, passwordDB))
+                                // Logg-in in actions
+                                showAlert(this, R.string.loggedIn);
+
+                            else
+                                showAlert(this, R.string.misMatchAccountInfo);
+                        }
+                    }
+                });
+            }
         });
 
         // Launch Register Page
         findViewById(R.id.buttonRegister).setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
-
         });
 
 
         // Launch forgot password
-        findViewById(R.id.forgotPasswordText).setOnClickListener(view -> showAlert(R.string.press_test));
+        findViewById(R.id.forgotPasswordText).setOnClickListener(view -> showAlert(this, R.string.press_test));
+
     }
 
-    // used to show dialogs
-    protected void showAlert(int messageId) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-        builder.setMessage(messageId)
-                .setPositiveButton("OK", null);
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    // Grabs the account from the dataBase
+    protected Task<DocumentSnapshot> checkAccount(String emailAddress, String dbName, FirebaseFirestore fs) {
+        final TaskCompletionSource<DocumentSnapshot> tcs = new TaskCompletionSource<>();
+        fs.collection(dbName)
+                .whereEqualTo("email", emailAddress)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                            DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+                            tcs.setResult(document);
+                        } else {
+                            showAlert(this, R.string.notAccountFound);
+                        }
+                    } else {
+                        showAlert(this, R.string.queryError);
+                    }
+                });
+
+        return tcs.getTask();
+    }
+
+    // Checks if password matches the one in the database
+    protected boolean checkPassword(String local, String database) {
+        return local.equals(database);
+    }
+
+
+    // Makes sure the fields are filled in
+    protected boolean checkInputs(String email, String password){
+        return !email.trim().isEmpty() && !password.trim().isEmpty();
     }
 }
