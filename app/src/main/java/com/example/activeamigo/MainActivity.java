02fc39a3e77e008184e8d2f4e880fae381415c13
mjalbert99 <mjalbert@ucsd.edu>
2024-02-27 11:45:28 -0800

    final button placement to sign out

--- app/src/main/java/com/example/activeamigo/MainActivity.java
+++ app/src/main/java/com/example/activeamigo/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.activeamigo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -56,18 +57,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-        // Launch activity_create_account.xml when the button is clicked
-        findViewById(R.id.launchCreateAccount).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        });
-
-        findViewById(R.id.launchLogOut).setOnClickListener(view -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
-        });
     }
 
     @Override
